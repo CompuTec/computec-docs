f0fe5031bc387516ea42aa46bb77544cb607c167
Sebastian Szczepański <contact@sszczep.dev>
2024-11-22 19:35:18 +0100

    feat: move version selector to navbar on mobile

--- src/theme/NavbarItem/DocsVersionDropdownNavbarItem.tsx
+++ src/theme/NavbarItem/DocsVersionDropdownNavbarItem.tsx
@@ -1,19 +1,118 @@
-import React from "react";
-
-import DocsVersionDropdownNavbarItem from "@theme-original/NavbarItem/DocsVersionDropdownNavbarItem";
-import { Props } from "@theme/NavbarItem/DocsVersionDropdownNavbarItem";
-
+import React from 'react';
 import {
+  useVersions,
+  useActiveDocContext,
+  useDocsVersionCandidates,
+  useDocsPreferredVersion,
   useActivePlugin,
   useDocsData,
-} from "@docusaurus/plugin-content-docs/client";
+} from '@docusaurus/plugin-content-docs/client';
+import {translate} from '@docusaurus/Translate';
+import {useLocation} from '@docusaurus/router';
+import DefaultNavbarItem from '@theme/NavbarItem/DefaultNavbarItem';
+import DropdownNavbarItem from '@theme/NavbarItem/DropdownNavbarItem';
+import type {Props} from '@theme/NavbarItem/DocsVersionDropdownNavbarItem';
+import type {LinkLikeNavbarItemProps} from '@theme/NavbarItem';
+import type {
+  GlobalVersion,
+  GlobalDoc,
+  ActiveDocContext,
+} from '@docusaurus/plugin-content-docs/client';
 
-export default function DocsVersionDropdownNavbarItemWrapper(props: Props) {
-  const docsData = useDocsData(props.docsPluginId);
+function getVersionMainDoc(version: GlobalVersion): GlobalDoc {
+  return version.docs.find((doc) => doc.id === version.mainDocId)!;
+}
+
+function getVersionTargetDoc(
+  version: GlobalVersion,
+  activeDocContext: ActiveDocContext,
+): GlobalDoc {
+  // We try to link to the same doc, in another version
+  // When not possible, fallback to the "main doc" of the version
+  return (
+    activeDocContext.alternateDocVersions[version.name] ??
+    getVersionMainDoc(version)
+  );
+}
+
+export default function DocsVersionDropdownNavbarItem({
+  mobile,
+  docsPluginId,
+  dropdownActiveClassDisabled,
+  dropdownItemsBefore,
+  dropdownItemsAfter,
+  ...props
+}: Props): JSX.Element {
+  const {search, hash} = useLocation();
+  const activeDocContext = useActiveDocContext(docsPluginId);
+  const versions = useVersions(docsPluginId);
+  const {savePreferredVersionName} = useDocsPreferredVersion(docsPluginId);
+
+  const docsData = useDocsData(docsPluginId);
   const activePlugin = useActivePlugin();
 
-  if (props.docsPluginId !== activePlugin?.pluginId) return null;
+  function versionToLink(version: GlobalVersion): LinkLikeNavbarItemProps {
+    const targetDoc = getVersionTargetDoc(version, activeDocContext);
+    return {
+      label: version.label,
+      // preserve ?search#hash suffix on version switches
+      to: `${targetDoc.path}${search}${hash}`,
+      isActive: () => version === activeDocContext.activeVersion,
+      onClick: () => savePreferredVersionName(version.name),
+    };
+  }
+
+  const items: LinkLikeNavbarItemProps[] = [
+    ...dropdownItemsBefore,
+    ...versions.map(versionToLink),
+    ...dropdownItemsAfter,
+  ];
+
+  const dropdownVersion = useDocsVersionCandidates(docsPluginId)[0];
+
+  // Mobile dropdown is handled a bit differently
+  const dropdownLabel =
+    mobile && items.length > 1
+      ? translate({
+          id: 'theme.navbar.mobileVersionsDropdown.label',
+          message: 'Versions',
+          description:
+            'The label for the navbar versions dropdown on mobile view',
+        })
+      : dropdownVersion.label;
+  const dropdownTo =
+    mobile && items.length > 1
+      ? undefined
+      : getVersionTargetDoc(dropdownVersion, activeDocContext).path;
+
+  // We don't want to render a version dropdown with 0 or 1 item. If we build
+  // the site with a single docs version (onlyIncludeVersions: ['1.0.0']),
+  // We'd rather render a button instead of a dropdown
+  // if (items.length <= 1) {
+  //   return null;
+  //   return (
+  //     <DefaultNavbarItem
+  //       {...props}
+  //       mobile={mobile}
+  //       label={dropdownLabel}
+  //       to={dropdownTo}
+  //       isActive={dropdownActiveClassDisabled ? () => false : undefined}
+  //     />
+  //   );
+  // }
+
+  if (docsPluginId !== activePlugin?.pluginId) return null;
   if (docsData.versions.length <= 1) return null;
 
-  return <DocsVersionDropdownNavbarItem {...props} />;
+  return (
+    <DropdownNavbarItem
+      {...props}
+      // mobile={mobile} // Don't display Version in sidebar menu
+      style={{ display: "block" }}
+      label={dropdownLabel}
+      to={dropdownTo}
+      items={items}
+      isActive={dropdownActiveClassDisabled ? () => false : undefined}
+    />
+  );
 }
