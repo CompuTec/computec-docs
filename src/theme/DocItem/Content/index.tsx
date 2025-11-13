56d12d7f9bae8fb73e8c80a7324e901615865da8
Joanna Sulżyńska <joanna.sulzynska@computec.pl>
2025-11-13 11:00:16 +0100

    update docs for CompuTec AppEngine 3.0 with SAP HANA SPS 08 support notice

--- src/theme/DocItem/Content/index.tsx
+++ src/theme/DocItem/Content/index.tsx
@@ -20,6 +20,13 @@ export default function ContentWrapper(props: Props): ReactNode {
     <>
       {activePlugin.pluginId === "appengine" && activeDocContext.activeVersion?.label === "3.0" && (
         <Admonition type="warning">
+          <p><b>Important Notice: SAP HANA SPS 08 Support Status</b></p>
+          <p>At this time, <b>AppEngine 3.0 does not yet support SAP HANA version SPS 08 (Service Pack Stack 08)</b>. We strongly recommend <b>waiting before installing or upgrading</b> to this version until we announce official support.</p>
+          <p>Our development team is actively working on adding compatibility with SAP HANA SPS 08, and we will update this article as soon as support is released.</p>
+        </Admonition>
+      )}
+      {activePlugin.pluginId === "appengine" && activeDocContext.activeVersion?.label === "3.0" && (
+        <Admonition type="note">
           <p>You are viewing documentation for <b>CompuTec AppEngine 3.0 (.NET 8)</b>.</p>
           <p>If you are working with older environments, you may be looking for <b>CompuTec AppEngine 2.0</b> based on .NET Framework, which is still supported: <a href="https://learn.computec.one/docs/appengine/2.0/">https://learn.computec.one/docs/appengine/2.0/</a></p>
           <p>As <b>CompuTec AppEngine 3.0</b> introduces a new architecture, all CompuTec components must be updated together.</p>
@@ -27,7 +34,7 @@ export default function ContentWrapper(props: Props): ReactNode {
         </Admonition>
       )}
       {activePlugin.pluginId === "appengine" && activeDocContext.activeVersion?.label === "2.0" && (
-        <Admonition type="warning">
+        <Admonition type="note">
           <p>You are viewing documentation for <b>CompuTec AppEngine 2.0 (.NET Framework)</b>, which is still supported.</p>
           <p>We recommend upgrading to <b>CompuTec AppEngine 3.0</b> built on .NET 8, which provides better performance and a modern, centralized installation process for CompuTec ProcessForce and other CompuTec solutions.</p>
           <p>However, please note that <b>CompuTec AppEngine 3.0</b> has a different architecture, and all CompuTec components need to be updated together.</p>
